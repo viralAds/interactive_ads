27f880fa6c7fd710d57d5d08c818dadaff621cc6
43215-ekaleidoAnuj <anuj.kumar@ekaleido.co>
2021-07-07 13:01:37 +0530

    Update Navbar.jsx

--- src/pages/Navbar/Navbar.jsx
+++ src/pages/Navbar/Navbar.jsx
@@ -33,7 +33,7 @@ function HomePage() {
             <Nav className="mr-auto" navbar>
               <UncontrolledDropdown nav inNavbar>
                 <DropdownToggle nav caret>
-                  WEDDING COLLECTION
+                  BLACKBERRYS
                 </DropdownToggle>
                 <DropdownMenu right>
                   <DropdownItem>
@@ -53,7 +53,7 @@ function HomePage() {
 
               <UncontrolledDropdown nav inNavbar>
                 <DropdownToggle nav caret>
-                  INTERACTIVE VIDEOS
+                  INTERACTIVE Ads
                 </DropdownToggle>
                 <DropdownMenu right>
                   <DropdownItem>
@@ -88,6 +88,88 @@ function HomePage() {
                   VIRAL ADS
                 </DropdownToggle>
                 <DropdownMenu right>
+                  <UncontrolledDropdown nav inNavbar>
+                    <DropdownToggle nav caret>
+                      AMD Ads
+                    </DropdownToggle>
+                    <DropdownMenu right>
+                      <DropdownItem>
+                        <Link to="/amd">AMD</Link>
+                      </DropdownItem>
+                      <DropdownItem>
+                        <Link to="/amd_flipkart">AMD Flipkart</Link>
+                      </DropdownItem>
+                      <DropdownItem>
+                        <Link to="/amd_amazon">AMD Amazon</Link>
+                      </DropdownItem>
+                      <DropdownItem>
+                        <Link to="/amd_merc_q2_2021">AMD Merc Q2 2021</Link>
+                      </DropdownItem>
+                      <DropdownItem>
+                        <Link to="/amd_merc_300x250_q2_2021">
+                          AMD Merc 300x250 Q2 2021
+                        </Link>
+                      </DropdownItem>
+                      <DropdownItem>
+                        <Link to="/amd_q2_commercials_320x480">
+                          AMD Q2 Commercials 320x480
+                        </Link>
+                      </DropdownItem>
+                      <DropdownItem>
+                        <Link to="/amd_q2_commercials_320x480_2">
+                          AMD Q2 Commercials 320x480_2
+                        </Link>
+                      </DropdownItem>
+                      <DropdownItem>
+                        <Link to="/amd_f1">AMD F1</Link>
+                      </DropdownItem>
+                    </DropdownMenu>
+                  </UncontrolledDropdown>
+
+                  <UncontrolledDropdown nav inNavbar>
+                    <DropdownToggle nav caret>
+                      Ghadi Ads
+                    </DropdownToggle>
+                    <DropdownMenu>
+                      <DropdownItem>
+                        <Link to="/ghadi">Ghadi</Link>
+                      </DropdownItem>
+                      <DropdownItem>
+                        <Link to="/ghadi_2">Ghadi Option 2</Link>
+                      </DropdownItem>
+                      <DropdownItem>
+                        <Link to="/ghadi_3">Ghadi Option 3</Link>
+                      </DropdownItem>
+                    </DropdownMenu>
+                  </UncontrolledDropdown>
+
+                  <UncontrolledDropdown nav inNavbar>
+                    <DropdownToggle nav caret>
+                      Blackberry Ads
+                    </DropdownToggle>
+                    <DropdownMenu>
+                      <DropdownItem>
+                        <Link to="/blackberrys_one">BLACKBERRYS 1</Link>
+                      </DropdownItem>
+                      <DropdownItem>
+                        <Link to="/blackberrys_three">BLACKBERRYS 2</Link>
+                      </DropdownItem>
+                    </DropdownMenu>
+                  </UncontrolledDropdown>
+
+                  <UncontrolledDropdown nav inNavbar>
+                    <DropdownToggle nav caret>
+                      Vega Ads
+                    </DropdownToggle>
+                    <DropdownMenu>
+                      <DropdownItem>
+                        <Link to="/vega_women">Vega Women</Link>
+                      </DropdownItem>
+                      <DropdownItem>
+                        <Link to="/vega">Vega</Link>
+                      </DropdownItem>
+                    </DropdownMenu>
+                  </UncontrolledDropdown>
                   <DropdownItem>
                     <Link to="/panasonic">PANASONIC</Link>
                   </DropdownItem>
@@ -103,61 +185,11 @@ function HomePage() {
                   <DropdownItem>
                     <Link to="/asics">ASICS</Link>
                   </DropdownItem>
-                  <DropdownItem>
-                    <Link to="/blackberrys_one">BLACKBERRYS 1</Link>
-                  </DropdownItem>
-                  <DropdownItem>
-                    <Link to="/blackberrys_three">BLACKBERRYS 2</Link>
-                  </DropdownItem>
-                  
-                  <DropdownItem>
-                    <Link to="/amd">AMD</Link>
-                  </DropdownItem>
-                  <DropdownItem>
-                    <Link to="/amd_flipkart">AMD Flipkart</Link>
-                  </DropdownItem>
-                  <DropdownItem>
-                    <Link to="/amd_amazon">AMD Amazon</Link>
-                  </DropdownItem>
-                  <DropdownItem>
-                    <Link to="/amd_merc_q2_2021">AMD Merc Q2 2021</Link>
-                  </DropdownItem>
-                  <DropdownItem>
-                    <Link to="/amd_merc_300x250_q2_2021">
-                      AMD Merc 300x250 Q2 2021
-                    </Link>
-                  </DropdownItem>
-                  <DropdownItem>
-                    <Link to="/amd_q2_commercials_320x480">
-                      AMD Q2 Commercials 320x480
-                    </Link>
-                  </DropdownItem>
-                  <DropdownItem>
-                    <Link to="/amd_q2_commercials_320x480_2">
-                      AMD Q2 Commercials 320x480_2
-                    </Link>
-                  </DropdownItem>
-                  <DropdownItem>
-                    <Link to="/amd_f1">AMD F1</Link>
-                  </DropdownItem>
+
                   <DropdownItem>
                     <Link to="/_24seven">24 Seven</Link>
                   </DropdownItem>
-                  <DropdownItem>
-                    <Link to="/ghadi">Ghadi</Link>
-                  </DropdownItem>
-                  <DropdownItem>
-                    <Link to="/ghadi_2">Ghadi Option 2</Link>
-                  </DropdownItem>
-                  <DropdownItem>
-                    <Link to="/ghadi_3">Ghadi Option 3</Link>
-                  </DropdownItem>
-                  <DropdownItem>
-                    <Link to="/vega_women">Vega Women</Link>
-                  </DropdownItem>
-                  <DropdownItem>
-                    <Link to="/vega">Vega</Link>
-                  </DropdownItem>
+
                   <DropdownItem>
                     <Link to="/envorincs">Envorincs</Link>
                   </DropdownItem>
