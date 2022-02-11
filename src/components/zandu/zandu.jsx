45f60a19f90713b7f8fa7192c11d13986a236f3a
43215Anuj <anuj.kumar@ekaleido.co>
2022-02-11 22:53:50 +0530

    google ads

--- src/components/zandu/zandu.jsx
+++ src/components/zandu/zandu.jsx
@@ -3,7 +3,19 @@ const Zandu = () => {
     <div className="_320x480frame">
       <iframe
         src="https://interactiveviralads.s3.ap-south-1.amazonaws.com/zandu320x480/index.html"
-        title="wildstone Advertisement"
+        title="zandu Advertisement"
+      ></iframe>
+    </div>
+  );
+};
+
+
+export const ZanduParallax = () => {
+  return (
+    <div className="_320x480frame">
+      <iframe
+        src="https://interactiveviralads.s3.ap-south-1.amazonaws.com/zanduparallax/index.html"
+        title="zandu Advertisement"
       ></iframe>
     </div>
   );
