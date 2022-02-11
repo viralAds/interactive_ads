9e997828768151f18f11dc659f27b08c03de042a
43215Anuj <anuj.kumar@ekaleido.co>
2022-02-11 23:05:31 +0530

    updated parallax

--- src/components/zandu/zandu.jsx
+++ src/components/zandu/zandu.jsx
@@ -9,14 +9,21 @@ const Zandu = () => {
   );
 };
 
-
 export const ZanduParallax = () => {
   return (
     <div className="_320x480frame">
-      <iframe
-        src="https://interactiveviralads.s3.ap-south-1.amazonaws.com/zanduparallax/index.html"
-        title="zandu Advertisement"
-      ></iframe>
+      <script
+        async
+        src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9602911244724790"
+        crossorigin="anonymous"
+      ></script>
+      <ins
+        class="adsbygoogle"
+        style="display:inline-block;width:360px;height:640px"
+        data-ad-client="ca-pub-9602911244724790"
+        data-ad-slot="2877036084"
+      ></ins>
+      <script>(adsbygoogle = window.adsbygoogle || []).push({});</script>
     </div>
   );
 };
